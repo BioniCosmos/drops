980f4b6d6701f435fbcd6824814d203dae4216ce
Daniel Ding <41363844+BioniCosmos@users.noreply.github.com>
2025-07-15 10:59:03 +0800

    Use Neon serverless driver

--- lib/server/db.ts
+++ lib/server/db.ts
@@ -1,15 +1,9 @@
+import { PrismaNeon } from '@prisma/adapter-neon'
 import { PrismaClient } from '@prisma/client'
+import 'dotenv/config'
 
-const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
-
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>
-} & typeof global
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+const connectionString = `${process.env.DATABASE_URL}`
+const adapter = new PrismaNeon({ connectionString })
+const prisma = new PrismaClient({ adapter })
 
 export default prisma
-
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
