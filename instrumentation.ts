59c861d28776cd89d983228f4d9241f21bd5a053
Daniel Ding <41363844+BioniCosmos@users.noreply.github.com>
2025-09-24 23:09:38 +0800

    Remove OpenTelemetry

--- instrumentation.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaInstrumentation } from '@prisma/instrumentation'
-import { registerOTel } from '@vercel/otel'
-
-export async function register() {
-  registerOTel({ instrumentations: [new PrismaInstrumentation()] })
-}
